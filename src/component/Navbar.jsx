ae77e6fa5f35bcc8f994bf2b31bfe69af561ed5e
Toth Zoltan <thotzee@gmail.com>
2022-10-07 17:02:53 +0200

    ready

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div>
         <nav className="navbar navbar-expand-lg bg-light py-3 shadow-sm">
   <div className="container">
-    <NavLink className="navbar-brand fw-bold fs-4" to="/">Tommy Aquarel's Fake Webshop</NavLink>
+    <NavLink className="navbar-brand fw-bold fs-4" to="/emart">Tommy Aquarel's Fake Webshop</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
